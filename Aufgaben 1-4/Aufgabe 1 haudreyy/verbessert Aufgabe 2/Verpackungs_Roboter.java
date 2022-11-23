b5847e93aa7a2758e8690e92d22e6ca808cd776e
haudreyy <117124617+haudreyy@users.noreply.github.com>
2022-11-23 14:25:47 +0100

    Produktionsmanager initialisiert

---
+++ Aufgaben 1-4/Aufgabe 1 haudreyy/verbessert Aufgabe 2/Verpackungs_Roboter.java
@@ -0,0 +1,33 @@
+
+/**
+ * Beschreiben Sie hier die Klasse Verpackungs_Roboter.
+ * 
+ * @author (Ihr Name) 
+ * @version (eine Versionsnummer oder ein Datum)
+ */
+public class Verpackungs_Roboter
+{
+    // Instanzvariablen - ersetzen Sie das folgende Beispiel mit Ihren Variablen
+    private int x;
+
+    /**
+     * Konstruktor für Objekte der Klasse Verpackungs_Roboter
+     */
+    public Verpackungs_Roboter()
+    {
+        // Instanzvariable initialisieren
+        x = 0;
+    }
+
+    /**
+     * Ein Beispiel einer Methode - ersetzen Sie diesen Kommentar mit Ihrem eigenen
+     * 
+     * @param  y    ein Beispielparameter für eine Methode
+     * @return        die Summe aus x und y
+     */
+    public int beispielMethode(int y)
+    {
+        // tragen Sie hier den Code ein
+        return x + y;
+    }
+}
