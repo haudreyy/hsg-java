c1b82a52c341f45ce127b7676d7d95f3820546ae
haudreyy <117124617+haudreyy@users.noreply.github.com>
2022-11-23 13:45:30 +0100

    thread Lieferant implementiert

---
+++ Aufgaben 1-4/Aufgabe 1 haudreyy/verbessert Aufgabe 2/Thread_Lieferant.java
@@ -0,0 +1,42 @@
+
+/**
+ * Beschreiben Sie hier die Klasse Thread_Lieferant.
+ * 
+ * @author (Ihr Name) 
+ * @version (eine Versionsnummer oder ein Datum)
+ */
+public class Thread_Lieferant extends Thread
+{
+    // Konstruktor
+    public Thread_Lieferant(String name) {
+        super(name);
+    }
+
+
+    @Override
+
+/* public void run()
+    {
+        System.out.println ("thread is running");
+        try 
+        {
+            Thread.sleep(2000);
+        } 
+        catch (Exception e) 
+        {
+            e.printStackTrace();
+        }
+        System.out.println ("thread is back");
+    } 
+*/
+
+    public static void main (String args[])
+    {
+
+    }
+
+
+
+
+
+}   
